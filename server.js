95772ae9b4873bc1742921970f7d1aa3e192150c
jumaelmartins <jumaelmartins@gmail.com>
2023-02-01 21:50:14 -0300

    T3

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
-app.use(express.static(path.resolve(__dirname, 'frontend')));
 
 const sessionOptions = session({
   secret: 'akasdfj0út23453456+54qt23qv  qwf qwer qwer qewr asdasdasda a6()',
