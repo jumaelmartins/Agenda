6d81cb512ad08d5371dae9bf4b6140c099511c68
jumaelmartins <jumaelmartins@gmail.com>
2023-02-01 21:58:16 -0300

    T4

--- server.js
+++ server.js
@@ -16,10 +16,15 @@ const helmet = require('helmet');
 const csrf = require('csurf');
 const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
 
-app.use(helmet());
+app.use(
+  helmet({
+      contentSecurityPolicy: false
+  })
+)
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(express.static(path.resolve(__dirname, 'frontend')));
 
 const sessionOptions = session({
   secret: 'akasdfj0út23453456+54qt23qv  qwf qwer qwer qewr asdasdasda a6()',
