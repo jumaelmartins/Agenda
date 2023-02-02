589b2de2aca4a09606a062ecad4cd85784593219
jumaelmartins <jumaelmartins@gmail.com>
2023-02-01 22:18:22 -0300

    t7

--- server.js
+++ server.js
@@ -50,8 +50,8 @@ app.use(csrfMiddleware);
 app.use(routes);
 
 app.on('pronto', () => {
-  app.listen(3001, () => {
-    console.log('Acessar http://localhost:3001');
+  app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
     console.log('Servidor executando na porta 3000');
   });
 });
